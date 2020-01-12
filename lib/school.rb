691370b9b01f5a48618daf231b3bd600aa46bf51
Martin Chaplin <martin.chaplin44@gmail.com>
2020-01-12 19:20:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,29 @@
 
 class School
-  attr_accessor :name, :roster
-  
-  
-  def intialize(name)
-    @name = name 
+  def initialize(name)
+    @name = name
     @roster = {}
-  end 
-add_student = {}
-  def add_student(student_name, grade)
-   
   end
-end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end 
